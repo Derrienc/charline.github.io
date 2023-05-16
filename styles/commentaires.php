aa44bdcc14353899c94ef9b3f0e42bdcb3265b83
Derrienc <118453531+Derrienc@users.noreply.github.com>
2023-05-16 13:52:54 +0200

    Update commentaires.php

--- styles/commentaires.php
+++ styles/commentaires.php
@@ -1,21 +1,17 @@
 <?php
     			// Vérifier si le formulaire a été soumis
     			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-				
         			// Récupérer les données du formulaire
         			$nom = $_POST["nom"];
         			$email = $_POST["email"];
         			$commentaire = $_POST["commentaire"];
-			        // Enregistrement du commentaire dans une base de données ou dans un fichier, etc.
-        			// Exemple de code pour enregistrer dans un fichier
+			        // Enregistrement du commentaire dans un fichier
        	 			$fichier = fopen("commentaires.txt", "a");
         			fwrite($fichier, "Nom: " . $nom . "\n");
         			fwrite($fichier, "Email: " . $email . "\n");
-        			fwrite($fichier, "Commentaire: " . $commentaire . "\n\n");
-        			fclose($fichier);
-
-        			// Affichage d'un message de succès
+        			fwrite($fichier, "Commentaire: " . $commentaire . "\n\n\n");
+        			fclose("commentaires.txt");
+        			// Affiche un message de succès, si commentaires bien enregistrer dans le fichier
         			echo "Merci pour votre commentaire!";
-        			exit(); // Arrêter l'exécution du script après l'affichage du message
 			}
     				?>
