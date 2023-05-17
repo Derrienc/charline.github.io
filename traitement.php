8711932bb0f65426b2a5d5d9b9b1f67142c3b980
Derrienc <118453531+Derrienc@users.noreply.github.com>
2023-05-17 08:04:17 +0200

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -1,17 +1,19 @@
 <?php
-    			// Vérifier si le formulaire a été soumis
-    			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        			// Récupérer les données du formulaire
-        			$nom = $_POST["nom"];
-        			$email = $_POST["email"];
-        			$commentaire = $_POST["commentaire"];
-			        // Enregistrement du commentaire dans un fichier
-       	 			$fichier = fopen("commentaires.txt", "a");
-        			fwrite($fichier, "Nom: " . $nom . "\n");
-        			fwrite($fichier, "Email: " . $email . "\n");
-        			fwrite($fichier, "Commentaire: " . $commentaire . "\n\n\n");
-        			fclose($fichier);
-				
-				echo "Merci pour votre commentaire!";
-			}
-    				?>
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+    	// Vérifier si le formulaire a été soumis
+    	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Récupérer les données du formulaire
+        	$nom = $_POST["nom"];
+        	$email = $_POST["email"];
+        	$commentaire = $_POST["commentaire"];
+		// Enregistrement du commentaire dans un fichier
+       	 	$fichier = fopen("commentaires.txt", "a");
+        	fwrite($fichier, "Nom: " . $nom . "\n");
+        	fwrite($fichier, "Email: " . $email . "\n");
+        	fwrite($fichier, "Commentaire: " . $commentaire . "\n\n\n");
+        	fclose($fichier);
+			
+		echo "Merci pour votre commentaire!";
+	}
+?>
